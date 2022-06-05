34035c8d928e2f15277a874031223702c3d6f279
Itay Perl <itay.perl@gmail.com>
2022-06-05 15:14:50 +0300

    modernize a bit

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from radical_dawg import RadicalDawg
 from dict_dawg import DictDawg
 
-from werkzeug.wrappers import Request, Response
-from werkzeug.serving import run_simple
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 import romkan
 
 import json
@@ -17,12 +18,29 @@ DATA_DIR = pathlib.Path(__file__).parent
 
 MAX_RESULTS = 50
 
-def lookup(rad_dawg, dict_dawg, pattern):
+app = Flask('kantan')
+CORS(app)
+
+
+print('Creating in-memory database (may take up to 30 seconds)...', end=' ') 
+
+with open(DATA_DIR / 'kradfile-u', 'r') as f:
+    rad_dawg = RadicalDawg.from_kradfile(f)
+
+with open(DATA_DIR / 'JMdict_e', 'r') as jmdict:
+    dict_dawg = DictDawg.from_jmdict(jmdict)
+
+print('Done!')
+
+
+@app.get('/')
+@app.get('/<pattern>')
+def lookup(pattern=''):
     components = []
 
     # sanity
     if len(pattern) > 40 and pattern.count('[') > 20:
-        return []
+        return jsonify([])
 
     # romaji => kana
     pattern = re.sub('[-A-Z]+', lambda m: romkan.to_katakana(m.group(0)), pattern)
@@ -35,40 +53,8 @@ def lookup(rad_dawg, dict_dawg, pattern):
         else:
             components.append(set([c]))
 
-    return dict_dawg.lookup_word(components)[:MAX_RESULTS]
+    return jsonify(dict_dawg.lookup_word(components)[:MAX_RESULTS])
 
-def json_service(host, port, handler):
-
-    @Request.application
-    def service(request):
-        data = handler(request.path[1:])
-
-        response = Response(json.dumps(data), mimetype='application/json')
-
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Methods', 'GET')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-
-        return response
-
-    run_simple(host, port, service)
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--port', type=int, default=4000)
-    args = parser.parse_args()
-
-    print('Creating in-memory database (may take up to 30 seconds)...', end=' ') 
-
-    with open(DATA_DIR / 'kradfile-u', 'r') as f:
-        rad_dawg = RadicalDawg.from_kradfile(f)
-
-    with open(DATA_DIR / 'JMdict_e', 'r') as jmdict:
-        dict_dawg = DictDawg.from_jmdict(jmdict)
-
-    print('Done!')
-
-    json_service('0.0.0.0', args.port, functools.partial(lookup, rad_dawg, dict_dawg))
 
 if __name__ == '__main__':
     main()
